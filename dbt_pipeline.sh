234ae9a8dfc39fad2fea868cff0c49a932112b35
Amit Kohli <amit.kohli@accesscharity.org.uk>
2026-05-21 11:28:16 +0100

    ci: upgrade dbt parse to compile; add git pull to dbt_pipeline.sh (re-apply after refactor)

--- dbt_pipeline.sh
+++ dbt_pipeline.sh
@@ -3,6 +3,9 @@
 # Use run_dbt.sh wrapper instead (handles timing + cc timeRun output).
 cd /srv/projects/dbt-asc
 
+# pull latest from main before running
+git pull --rebase origin main
+
 dbt deps  > logs/dbt_run.log 2>&1
 dbt run --target prod >> logs/dbt_run.log 2>&1
 dbt test  >> logs/dbt_run.log 2>&1
